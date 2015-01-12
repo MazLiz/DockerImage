422b7c696c2bdd7987ff63639f3fb4834953c965
MazLiz <lisa.mazzon@gmail.com>
2015-01-12 17:27:00 +0100

    cambio start fuseki-jenkins

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-echo "prova script configurazione"
-java -jar /usr/share/jenkins/jenkins.war &
 cd /jena-fuseki-1.1.1
 nohup ./fuseki-server --update --port 3030 --loc /tmp/fusekidir /modaclouds/kb > /tmp/fuseki.log 2>&1 &
-cd ..
+
+java -jar /usr/share/jenkins/jenkins.war &
+
+
