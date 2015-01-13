9cd9f87f3fd13ad0969dac2d2102dafab8b90365
MazLiz <lisa.mazzon@gmail.com>
2015-01-13 12:06:29 +0100

    tolto jenkins in background

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,8 @@ cd /jena-fuseki-1.1.1
 pwd
 nohup ./fuseki-server --update --port 3030 --loc /tmp/fusekidir /modaclouds/kb > /tmp/fuseki.log 2>&1 &
 echo "ho acceso fuseki"
-
-java -jar /usr/share/jenkins/jenkins.war &
+sleep 10s
+ps aux | grep java
+java -jar /usr/share/jenkins/jenkins.war
 
 
