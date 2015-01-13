0cade370b577db6211f1fca9a8cd8207bdf01708
MazLiz <lisa.mazzon@gmail.com>
2015-01-13 12:01:36 +0100

    altri check

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,9 @@
 echo "Sono nel file sh"
 pwd
 cd /jena-fuseki-1.1.1
+pwd
 nohup ./fuseki-server --update --port 3030 --loc /tmp/fusekidir /modaclouds/kb > /tmp/fuseki.log 2>&1 &
+echo "ho acceso fuseki"
 
 java -jar /usr/share/jenkins/jenkins.war &
 
