e0dc48f3aa870e40bbaa29ad973b90e82e0f7040
MazLiz <lisa.mazzon@gmail.com>
2015-01-13 16:56:09 +0100

    tolti i commenti

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,6 @@
 #! /bin/sh
-echo "Sono nel file sh"
-pwd
 cd /jena-fuseki-1.1.1
-pwd
 nohup ./fuseki-server --update --port 3030 --loc /tmp/fusekidir /modaclouds/kb > /tmp/fuseki.log 2>&1 &
-echo "ho acceso fuseki"
-sleep 10s
-ps aux | grep java
 java -jar /usr/share/jenkins/jenkins.war
 
 
